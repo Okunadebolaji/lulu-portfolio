42234b914bec5be2119ee7b3b5ab61dcd1150c55
Okunade Habeeb <okunadehabeeb34@gmail.com>
2026-05-23 18:46:39 +0100

    Enable Swagger in production

--- Lulu_Portfolio.API/Program.cs
+++ Lulu_Portfolio.API/Program.cs
@@ -116,11 +116,8 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseStaticFiles();
 
